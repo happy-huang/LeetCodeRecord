19a6a02969d94ff47d2037f88b8b2e39c68d6a99
happy-huang <56758079+happy-huang@users.noreply.github.com>
2022-01-29 19:57:27 +0800

    Update 543. number-of-provinces.cpp

--- 543. number-of-provinces.cpp
+++ 543. number-of-provinces.cpp
@@ -65,3 +65,41 @@ public:
 };
 
 
+// 连通图
+class Solution {
+public:
+    int Find(vector<int> &parent, int i) {
+        if (parent[i] != i) {
+            parent[i] = Find(parent, parent[i]);
+        }
+        return parent[i];
+    }
+
+    void Union(vector<int> &parent, int i, int j) {
+        parent[Find(parent,i)] = Find(parent, j);
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> parent(n);
+        int ans=0;
+
+        for (int i=0; i<n; ++i) {
+            parent[i] = i;
+        }
+
+        for (int i=0; i<n; ++i) {
+            for (int j=0; j<n; ++j) {
+                if (isConnected[i][j] == 1) {
+                    Union(parent, i, j);
+                }
+            }
+        }
+        
+        for (int i=0; i<n; ++i) {
+            if (parent[i] == i) ++ans;
+        }
+        return ans;
+    }
+};
+
